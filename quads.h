66bf648b2061da5d52d823bc7f069c5d69237130
MeiS2 <62144700+MeiS2@users.noreply.github.com>
2020-04-05 18:55:56 +0300

    Add files via upload

--- quads.h
+++ quads.h
@@ -1,11 +1,16 @@
+int total=1;
+#define EXPAND_SIZE 40
+#define CURR_SIZE (total*EXPAND_SIZE)
+#define NEW_SIZE (EXPAND_SIZE + CURR_SIZE)
+
 enum iopcode{	
-	assign, add, sub, mul, div, mod,
+	assign, add, sub, mul, DIV, mod,
 	uminus, and, or, not, if_eq, if_noteq,
 	if_lesseq, if_geatereq,	if_less,
 	if_greater, jump, call,
-	param, return, getretval,
+	param, Return, getretval,
 	funcstart, funcend, tablecreate,
-	tablegetelem, tablesetelem	
+	tablegetelem, tablesetelem
 };
 
 enum expr_t{
@@ -38,7 +43,29 @@ struct quad{
 	struct expr* result;
 	struct expr* arg1;
 	struct expr* arg2;
-	unsigned label;
-	unsigned line;
+	unsigned int label;
+	unsigned int line;
 };
 
+
+typedef struct quad *temptq;
+temptq *quadtable;
+
+void tablequadcreation(){
+	int i;
+	quadtable =malloc(EXPAND_SIZE * sizeof(temptq));
+	for(i=0;i<40;i++) quadtable[i]=NULL;
+	quadtable[20]=malloc(sizeof(struct quad));
+	quadtable[20]->line=5;
+	printf("\n%d\n",quadtable[20]->line);
+}
+void addtotablequad(){
+	int i;
+	quadtable=realloc(quadtable, NEW_SIZE * sizeof(temptq)); 
+	for(i=CURR_SIZE;i<NEW_SIZE;i++) quadtable[i]=NULL;
+	quadtable[70]=malloc(sizeof(struct quad));
+	quadtable[70]->line=20;
+	printf("\n%d\n",quadtable[20]->line);
+	printf("\n%d\n",quadtable[70]->line);
+	total++;
+}
