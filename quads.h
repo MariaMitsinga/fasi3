36655ef8dff4568a52eccbe8b1df4af1326276d3
MariaMitsinga <62143657+MariaMitsinga@users.noreply.github.com>
2020-04-08 19:22:44 +0300

    arketa quads (error sto == kai !=)

--- quads.h
+++ quads.h
@@ -1,4 +1,5 @@
 int total=1;
+int tablecounter=0;
 #define EXPAND_SIZE 40
 #define CURR_SIZE (total*EXPAND_SIZE)
 #define NEW_SIZE (EXPAND_SIZE + CURR_SIZE)
@@ -6,7 +7,7 @@ int total=1;
 enum iopcode{	
 	assign, add, sub, mul, DIV, mod,
 	uminus, and, or, not, if_eq, if_noteq,
-	if_lesseq, if_geatereq,	if_less,
+	if_lesseq, if_greatereq, if_less,
 	if_greater, jump, call,
 	param, Return, getretval,
 	funcstart, funcend, tablecreate,
@@ -22,7 +23,7 @@ enum expr_t{
 	boolexpr_e,
 	assignexpr_e,
 	newtable_e,
-	costnum_e,
+	constnum_e,
 	constbool_e,
 	conststring_e,
 	nil_e
@@ -56,20 +57,76 @@ void tablequadcreation(){
 	int i;
 	quadtable =malloc(EXPAND_SIZE * sizeof(temptq));
 	for(i=0;i<40;i++) {
-		quadtable[i]=malloc(sizeof(struct quad));
-		quadtable[i]=NULL;
+		quadtable[i]=NULL;	
 	}
+}
+
+/*void domalloc(int size){
+	quadtable[size]=malloc(sizeof(struct quad));
+}*/
+
+void addtofunctionoffset(int size,enum iopcode op,struct expr* result,struct expr* arg1,struct expr* arg2,unsigned int label,unsigned int line){
+	quadtable[size]=malloc(sizeof(struct quad));
+	quadtable[size]->op=op;
+	quadtable[size]->result=result;
+	quadtable[size]->arg1=arg1;
+	quadtable[size]->arg2=arg2;
+	quadtable[size]->label=label;
+	quadtable[size]->line=line;
 }
 
 void realloctablequad(){
 	int i;
 	quadtable=realloc(quadtable, NEW_SIZE * sizeof(temptq)); 
 	for(i=CURR_SIZE;i<NEW_SIZE;i++){
-		quadtable[i]=malloc(sizeof(struct quad));
 		quadtable[i]=NULL;
 	}
 	total++;
 }
+
+const char* getiopcode(enum iopcode op){
+	switch (op){
+      		case assign:		return "assign";
+		case add: 		return "add";
+		case sub: 		return "sub";
+		case mul: 		return "mul";
+		case DIV: 		return "div";
+		case mod: 		return "mod";
+		case uminus: 		return "uminus";
+		case and: 		return "and";
+		case or: 		return "or"; 
+		case not: 		return "not";
+		case if_eq: 		return "if_eq";
+		case if_noteq: 		return "if_noteq";
+		case if_lesseq: 	return "if_lesseq";
+		case if_greatereq: 	return "if_greatereq";	
+		case if_less: 		return "if_less";
+		case if_greater: 	return "if_greater";
+		case jump: 		return "jump";
+		case call: 		return "call";
+		case param: 		return "param";
+		case Return: 		return "return";
+		case getretval: 	return "getretval";
+		case funcstart: 	return "funcstart";
+		case funcend: 		return "funcend";
+		case tablecreate: 	return "tablecreate";
+		case tablegetelem: 	return "tablegetelem";
+		case tablesetelem: 	return "tablesetelem";
+   	}
+}
+
+
+void printfunstionoffset(){
+	int i=0;
+	while(tablecounter>i){
+		if(quadtable[i]->arg2!=NULL)
+			printf("\n%s %s %.0f %.0f",getiopcode(quadtable[i]->op),quadtable[i]->result->sym->name,quadtable[i]->arg1->numConst,quadtable[i]->arg2->numConst);
+		else
+			printf("\n%s %s %.0f",getiopcode(quadtable[i]->op),quadtable[i]->result->sym->name,quadtable[i]->arg1->numConst);
+		i++;
+	}
+}
+
 
 void functionoffsetcreation(){
 	int i=0;
@@ -84,30 +141,19 @@ struct expr* newexpr(enum expr_t type)
 	return tmp;
 }
 
-int CreateSecretVar(int counter, int scope, int yylineno,int funcounter,int functionoffset[],char* space){
+struct SymTableEntry * CreateSecretVar(int counter, int scope, int yylineno,int funcounter,int functionoffset[],char* space){
 	int i;
 	char* name=(char *)malloc(sizeof(char));
 	char* num=(char *)malloc(sizeof(char));	
-	struct SymTableEntry *tmp,*tmp2;	
-			
-	//do{
-		
-		sprintf(name, "%s", "_t");
-		sprintf(num, "%d", counter);			
-		strcat(name,num);
-		counter++; 
-		//tmp=NameLookUpInScope(ScopeTable,scope,name);
-		//for (i=scope; i>-1;i--){
-			//tmp2=NameLookUpInScope(ScopeTable,i,name);
-			//if (tmp2!=NULL)
-				//break;
-		//}
-	//}while(!(tmp==NULL && tmp2==NULL));
-	//}while(tmp!=NULL);
+	struct SymTableEntry *tmp,*tmp2;		
+	sprintf(name, "%s", "_t");
+	sprintf(num, "%d", counter);			
+	strcat(name,num);
+	//counter++; 
 	
-	insertNodeToHash(Head,name,"hidden variable",scope,yylineno,functionoffset[funcounter],space,1);
+	tmp=insertNodeToHash(Head,name,"hidden variable",scope,yylineno,functionoffset[funcounter],space,1);
 	free(name);
 	free(num);
 	functionoffset[funcounter]=functionoffset[funcounter]+1;
-	return counter;
+	return tmp;
 }
